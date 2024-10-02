c9271a89b3f7aceaa08224a39e2418dc61fb4878
NikolaySkotnikov <Ocean3001@yandex.ru>
2024-10-02 14:12:29 +0300

    django-extensions

--- blog_app/models.py
+++ blog_app/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.utils.text import slugify
+from unidecode import unidecode
 
-# Create your models here.
+
+class Post(models.Model):
+
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    slug = models.SlugField(unique=True)
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    data = models.JSONField(null=True, blank=True, default=list)
+    views = models.IntegerField(default=0)
+    published_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        slug = slugify(unidecode(self.title))
+        self.slug = slug
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("post_by_slug", args=[str(self.slug)])
--- blog_app/views.py
+++ blog_app/views.py
@@ -1,9 +1,7 @@
-from lib2to3.fixes.fix_input import context
-
 from django.core.paginator import Paginator
-from django.http import HttpResponse
 from django.shortcuts import render
 from .dataset import dataset
+from .models import Post
 
 menu = [
     {"name": "Главная", "alias": "main"},
@@ -12,16 +10,13 @@ menu = [
 ]
 
 
-def data_sort(x):
-    return x['views']
-
 def index(request):
 
     context = {
         'menu': menu,
         'page_alias': 'main',
-        'last_posts': dataset[-3:],
-        'popular_posts': sorted(dataset, key=data_sort, reverse=True)[:3],
+        'last_posts': Post.objects.all().order_by('-published_date')[:3],
+        'popular_posts': Post.objects.all().order_by('-views')[:3],
     }
 
     return render(request, 'main.html', context=context)
@@ -29,7 +24,6 @@ def index(request):
 
 def about(request):
     context = {
-        'posts': dataset,
         'menu': menu,
         'page_alias': 'about',
     }
@@ -38,12 +32,14 @@ def about(request):
 
 def blog(request):
 
-    paginator = Paginator(dataset, 3)
+    posts = Post.objects.all()
+
+    paginator = Paginator(posts, 3)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
 
     context = {
-        'posts': dataset,
+        'posts': posts,
         'menu': menu,
         'page_alias': 'blog',
         'page_obj': page_obj,
@@ -52,5 +48,9 @@ def blog(request):
 
 
 def post_by_slug(request, post_slug):
-    post = [post for post in dataset if post['slug'] == post_slug]
-    return render(request, 'blog_app/post_detail.html', context={'post':post[0], 'menu': menu})
+
+    post = Post.objects.get(slug=post_slug)
+    context = {'post': post,
+               'menu': menu}
+
+    return render(request, 'blog_app/post_detail.html', context=context)
