2cdcc1e9ad6077620ca68dbf820481351620e465
NikolaySkotnikov <Ocean3001@yandex.ru>
2024-10-14 09:27:18 +0300

    django dz4

--- python_blog/urls.py
+++ python_blog/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from blog_app.views import index, about
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,3 +10,7 @@ urlpatterns = [
     path('about/', about, name='about'),
     path('blog/', include('blog_app.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
